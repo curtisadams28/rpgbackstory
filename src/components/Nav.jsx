7c6c2ab45ef608aecc8561f44054a5b2f7bb810c
Curtis Adams <curtisadams28@gmail.com>
2020-10-08 20:15:30 +1300

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/nav.scss';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="nav"><h1 className="headerMain">rpgbackstory.</h1></div>
+    );
+  }
+}
+
+export default Nav;
